71a45e19ae8b54a08544a3c499e3375d8d0057c4
GoldEEr17 <108426205+GoldEEr17@users.noreply.github.com>
2026-03-11 17:12:04 +0100

    Add initial structure for Scrabble game classes

--- main_scrabble.py
+++ main_scrabble.py
@@ -4,8 +4,29 @@ def get_dictionnaire(choix="français"):
     pass
 
 
-DICTIONNAIRE = get_dictionnaire("français")
-NB_LIGNES, NB_COLONNES = 15,15
+def scrabble_initial_standard() :
+    #TO DO
+    DICTIONNAIRE = get_dictionnaire("français")
+    NB_LIGNES, NB_COLONNES = 15,15
+    ...
+
+    return Scrabble(...)
+
+
+
+
+class Tuile():
+    """
+    classe tuile représentant entièrement une tuile :
+        
+    - symbole : ex: "B"," " pour un joker
+    - valeur : ex: 3
+    """
+    
+class Chevalet():
+    """
+    classe Chevalet représentant 
+    """
 
 
 
@@ -15,6 +36,26 @@ class Scrabble():
     
     Variables contenu :
         
-    - grille_lettres : matrice numpy contenant '' si la case est non 
+    - TAILLE = (n,m) couple nb lignes/colonnes
+    - grille_lettres : matrice numpy de taille nxm contenant une Tuile ou None si la case est vide
+    - GRILLE_MODIFS : matrice numpy de taille nxm contenant ("MULT_LETTRE",k) ou ("MULT_MOT",k) ou ('',0) sinon
+    
+    - NB_TUILES_INITIAL
+    - nb_tuiles_sac 
+    
+    - TUILES_INITIALES : liste de NB_TUILES_INITIAL Tuiles
+    - tuiles_sacs : liste de nb_tuiles_sac Tuiles, celles contenues dans le sac
+    
+    - NB_JOUEURS (seront numérotés de 0 à NB_JOUEURS - 1)
+    - prochain_joueur
+    
+    - chevalets : liste de NB_JOUEURS Chevalets
+    
+    
+    
+    
+    
+    
+    
     """ 
     
