a5eed2c0a0948efb5bd2e16544b3fe8257f0f221
GoldEEr17 <108426205+GoldEEr17@users.noreply.github.com>
2026-03-11 16:08:38 +0100

    Add files via upload

---
+++ main_scrabble.py
@@ -0,0 +1,20 @@
+
+def get_dictionnaire(choix="français"):
+    # TO DO
+    pass
+
+
+DICTIONNAIRE = get_dictionnaire("français")
+NB_LIGNES, NB_COLONNES = 15,15
+
+
+
+class Scrabble():
+    """
+    Classe encodant l'état complet jeu.
+    
+    Variables contenu :
+        
+    - grille_lettres : matrice numpy contenant '' si la case est non 
+    """ 
+    
